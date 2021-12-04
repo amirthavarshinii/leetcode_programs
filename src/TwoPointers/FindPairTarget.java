502f2c6105e66c469080f7dffb9f9a64bc8b9d1c
amirthavarshini <socaklingambama.a@northeastern.edu>
2021-12-04 14:15:16 -0500

    finding count of pairs which leads the sum

---
+++ src/TwoPointers/FindPairTarget.java
@@ -0,0 +1,56 @@
+package TwoPointers;
+
+/**
+ * Given a sorted array A (sorted in ascending order),
+ * having N integers, find if there exists any pair of elements (A[i], A[j]) such that their sum is equal to X.
+ * {10, 20, 35, 50, 75, 80}
+ * X = 70
+ * output:
+ * 1
+ * @link https://www.geeksforgeeks.org/two-pointers-technique/
+ */
+public class FindPairTarget {
+
+
+  public int findCountOfPairSum(int[] arr, int target){
+
+    int left = 0;
+    int right = arr.length -1 ;
+    int countOfPairs = 0;
+    /**
+     * why are we doing left < right ??
+     *
+     */
+    while(left < right){
+      int sum = 0;
+      sum += arr[left];
+      sum += arr[right];
+      if(sum == target){
+        countOfPairs += 1;
+        left++;
+        right --; // can't use same element twice so once two elements
+        //are forming a pair , they cant be used again therefore its
+        //safe to skip both of these.
+      }
+      else if(sum < target){
+        left++;
+      }else{
+        right --;
+      }
+
+
+    }
+
+    return countOfPairs;
+  }
+
+
+  public static void main(String args[]){
+
+    FindPairTarget obj = new FindPairTarget();
+    int[] arr = new int[]{10,20,35,50,75,80};
+    System.out.println(obj.findCountOfPairSum(arr,70));
+  }
+
+
+}
